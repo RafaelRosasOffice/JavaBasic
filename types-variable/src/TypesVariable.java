112f4dae6e7edcf38952069b0b28526ab6f54ed4
Rafael Rosas <rafaelrosasoffice@gmail.com>
2023-03-15 12:01:42 +0000

    Class String

--- types-variable/src/TypesVariable.java
+++ types-variable/src/TypesVariable.java
@@ -1,5 +1,45 @@
 public class TypesVariable {
+    /*
+     * Estudar tipos primitivos;
+     * Estudar classe String que representa texto na aplicação.
+     */
+
+    /*
+     * Oito tipos primitivos
+     * int - inteiro
+     * byte - inteiro
+     * short - inteiro
+     * long - inteiro (precisa terminar com L. Ex: long CPF = 221234034L)
+     * float - fracionado (precisa terminar com F. Ex: float PI = 3.14F)
+     * double - fracionado
+     * boolean
+     * char
+     */
+
+    /*
+     * Para declarar uma constante usa-se a palavra reservada "final", seguida do
+     * tipo e o nome em caixa alta.
+     * Ex: final double VALOR_DE_PI = 3.14;
+     */
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    // Estudo da class String
+
+    /*
+     * public static void main(String[] args) {
+     * char[] arrayCarac = { 'R', 'a', 'f', 'a', ' ', 'R', 'o', 's', 'a', 's' };
+     * String txt = new String("Rafael Rosas");
+     * // String txt4 = "Rafael Rosas";
+     * 
+     * // Três formas de Contrutores da Classe String
+     * String tex1 = new String();
+     * String tex2 = new String(txt);
+     * String tex3 = new String(arrayCarac);
+     * 
+     * System.out.printf("tex1 = %s%ntex2 = %s%ntex3 = %s%n, tex1, tex2, tex3");
+     * }
+     */
 }
